b745848308925e6df0422c36f13cfef88e2907bd
ksha <ksha@ibowsoft.com>
2024-02-15 17:06:57 +0900

    dev3 추가기능 수정

--- gitTest/src/gitTest/AdditionalFeature.java
+++ gitTest/src/gitTest/AdditionalFeature.java
@@ -4,6 +4,7 @@ public class AdditionalFeature {
 	public AdditionalFeature() {}
 	
 	public void feature1() {
-		System.out.println("11111");
+		//System.out.println("11111");
+		System.out.println("22222"); //update
 	}
 }
